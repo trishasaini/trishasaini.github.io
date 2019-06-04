f6b21fe1e3ae1c4e226dc8415dbe5de0012dab60
Trisha <trishafsaini@gmail.com>
2019-06-04 02:16:02 -0700

    53- with logos inserted in widget

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -39,7 +39,11 @@ xhttp.onreadystatechange = function() {
        <h3 class="left_align">Time: ${parsedJson[0].match_time}</h3>
        <h3 class="right_align">Date: ${parsedJson[0].match_date}</h3>
       </div>
-      
+      <div class="block_container">
+      <img class="left_align club-logo" src="assets/img/${parsedJson[0].match_hometeam_name}.png" alt="${parsedJson[0].match_hometeam_name}" </img>
+      <img class="right_align club-logo" src="assets/img/${parsedJson[0].match_awayteam_name}.png" alt="${parsedJson[0].match_awayteam_name}" </img>
+     </div>
+     <br>
        <div class="block_container">
         <h2 class="home_team_score left_align">${parsedJson[0].match_hometeam_name}: ${parsedJson[0].match_hometeam_score}</h2>
         <h2 class="away_team_score right_align">${parsedJson[0].match_awayteam_name}: ${parsedJson[0].match_awayteam_score}</h2>
@@ -58,7 +62,11 @@ xhttp.onreadystatechange = function() {
        <h3 class="left_align">Time: ${parsedJson[1].match_time}</h3>
        <h3 class="right_align">Date: ${parsedJson[1].match_date}</h3>
       </div>
-      
+      <div class="block_container">
+      <img class="left_align club-logo" src="assets/img/${parsedJson[1].match_hometeam_name}.png" alt="${parsedJson[1].match_hometeam_name}" </img>
+      <img class="right_align club-logo" src="assets/img/${parsedJson[1].match_awayteam_name}.png" alt="${parsedJson[1].match_awayteam_name}" </img>
+     </div>
+     <br>
        <div class="block_container">
         <h2 class="home_team_score left_align">${parsedJson[1].match_hometeam_name}: ${parsedJson[1].match_hometeam_score}</h2>
         <h2 class="away_team_score right_align">${parsedJson[1].match_awayteam_name}: ${parsedJson[1].match_awayteam_score}</h2>
@@ -76,7 +84,11 @@ xhttp.onreadystatechange = function() {
        <h3 class="left_align">Time: ${parsedJson[2].match_time}</h3>
        <h3 class="right_align">Date: ${parsedJson[2].match_date}</h3>
       </div>
-      
+      <div class="block_container">
+      <img class="left_align club-logo" src="assets/img/${parsedJson[2].match_hometeam_name}.png" alt="${parsedJson[2].match_hometeam_name}" </img>
+      <img class="right_align club-logo" src="assets/img/${parsedJson[2].match_awayteam_name}.png" alt="${parsedJson[2].match_awayteam_name}" </img>
+     </div>
+     <br>
        <div class="block_container">
         <h2 class="home_team_score left_align">${parsedJson[2].match_hometeam_name}: ${parsedJson[2].match_hometeam_score}</h2>
         <h2 class="away_team_score right_align">${parsedJson[2].match_awayteam_name}: ${parsedJson[2].match_awayteam_score}</h2>
